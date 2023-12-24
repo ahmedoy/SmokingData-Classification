fe1e7846c68005fe70e07283f22429f02942e203
AnnTarek <es_Ann.Tarek@alexu.edu.eg>
2023-12-24 16:35:20 +0200

    fixed a bug in useful functions

--- useful_functions.py
+++ useful_functions.py
@@ -66,37 +66,45 @@ def get_normalized_train():
     sqaure_root_transformed_data, log_transformed_data, box_cox_transformed_data, yeo_johnson_transformed_data = get_tranformations(features[0])
     #scaling the data using standard scaler
     scaler = StandardScaler()
+    scaler.fit(box_cox_transformed_data)
     scaled_hemoglobin_train = scaler.transform(box_cox_transformed_data)
+
 
     sqaure_root_transformed_data, log_transformed_data, box_cox_transformed_data, yeo_johnson_transformed_data = get_tranformations(features[1])
     #scaling the data using standard scaler
     scaler = StandardScaler()
+    scaler.fit(box_cox_transformed_data)
     scaled_fasting_blood_sugar_train = scaler.transform(box_cox_transformed_data)
 
     #scaling the data using standard scaler usnig original data
     scaler = StandardScaler()
     LDL_feature = features[2].reshape(-1, 1)
+    scaler.fit(LDL_feature)
     scaled_LDL_train = scaler.transform(LDL_feature)
 
     #scaling the data using min-max scaler using original data
     scaler = MinMaxScaler()
     height_feature = features[3].reshape(-1, 1)
+    scaler.fit(height_feature)
     scaled_height_train = scaler.transform(height_feature)
 
     #scaling the data using robust scaler beacause it is robust to outliers and can handle them
     scaler = RobustScaler()
     weight_feature = features[4].reshape(-1, 1)
+    scaler.fit(weight_feature)
     scaled_weight = scaler.transform(weight_feature)
 
     sqaure_root_transformed_data, log_transformed_data, box_cox_transformed_data, yeo_johnson_transformed_data = get_tranformations(features[5])
     log_transformed_data = log_transformed_data.reshape(-1, 1)
     #scaling the data using min-max scaler
     scaler = MinMaxScaler()
+    scaler.fit(log_transformed_data)
     scaled_serum_creatinine = scaler.transform(log_transformed_data)
 
     sqaure_root_transformed_data, log_transformed_data, box_cox_transformed_data, yeo_johnson_transformed_data = get_tranformations(features[6])
     #scaling the data using standard scaler
     scaler = StandardScaler()
+    scaler.fit(box_cox_transformed_data)
     scaled_Gtp = scaler.transform(box_cox_transformed_data)
 
     return scaled_hemoglobin_train, scaled_fasting_blood_sugar_train, scaled_LDL_train, scaled_height_train, scaled_weight, scaled_serum_creatinine, scaled_Gtp
